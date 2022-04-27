b3a50b75b4750405050051ffac0cb19572e4da4d
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2022-04-27 23:08:01 +0300

    chore(render): remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,4 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(
-  <React.StrictMode>
-    <AppContainer />
-  </React.StrictMode>
-);
+root.render(<AppContainer />);
